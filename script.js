93a72e207dd578ea83fc5b29e22b41adb4e1965e
Dat P Nguyen <darkexpert3@gmail.com>
2024-07-30 02:17:10 +0700

    add function to generate random Y coordinate for ball

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const computerScore = document.querySelector('[data-computer-score]')
 const playerBar = document.querySelector('[data-player-bar]')
 const computerBar = document.querySelector('[data-computer-bar]')
 
+const ball = document.querySelector('[data-ball]')
+
 const body = document.querySelector('body')
 const bodySizeAndPosition = body.getBoundingClientRect()
 
@@ -20,3 +22,21 @@ body.addEventListener('mousemove', (event) => {
   const mouseY = event.pageY
   playerBar.style.top = mouseY.toString() + 'px'
 })
+
+const ballSizeAndPosition = ball.getBoundingClientRect()
+
+const ballCenterX = ballSizeAndPosition.width / 2 + ballSizeAndPosition.x
+const ballCenterY = ballSizeAndPosition.height / 2 + ballSizeAndPosition.y
+
+function generateBall() {
+  const randomY = Math.floor(
+    Math.random() * (bodySizeAndPosition.height - ballSizeAndPosition.height)
+  )
+
+  ball.style.top = randomY.toString() + 'px'
+}
+
+console.log(bodySizeAndPosition.height)
+generateBall()
+
+const ballWidth = getComputedStyle(ball).getPropertyValue('--ball-width')
